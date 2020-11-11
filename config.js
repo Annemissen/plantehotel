f3a2f059fa6d5d5fefb3652ea2386d0ad3269275
GrothenKek <65117443+GrothenKek@users.noreply.github.com>
2020-11-11 10:51:34 +0100

    models

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
-    databaseURI: 'mongodb://localhost/', // or MongoDB Atlas connection URI
+    databaseURI: "mongodb+  srv://Grothen:p4ndek4gek0ngen@cluster0.abwhj.mongodb.net/PlantsDB?retryWrites=true&w=majority",
+   // databaseURI: 'mongodb://localhost/', // or MongoDB Atlas connection URI
     localPort: 8080
 };
 
