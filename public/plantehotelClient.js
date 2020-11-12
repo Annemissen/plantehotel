7f9c4081cc76e462a36eefd5c2541d50d3a035b0
Annemissen <annemiegschulz@gmail.com>
2020-11-12 10:51:37 +0100

    planter kommer også ind i et array når de bliver tilføjet

--- public/plantehotelClient.js
+++ public/plantehotelClient.js
@@ -1,10 +1,14 @@
+let plants = []
+
 const addPlant = async () => {
     let plantNameInputField = document.getElementById("plantName");
     let amountOfPlantsInputField = document.getElementById("numberOfPlants");
 
     let plantName = plantNameInputField.value;
     let amountOfPlants = amountOfPlantsInputField.value;
-
+    let plantString = plantName + ', ' + amountOfPlants + ' stk.';
+    plants.push(plantString);
+    console.log(plants);
 
     if (plantName && amountOfPlants){
         let selector = document.getElementById("planter");
@@ -34,9 +38,17 @@ async function getText(url) {
     return await respons.text();
 }
 
+function setDateRestrictions(){
+    let datePicker = document.getElementById("datepicker");
+    datePicker.min = "2020-10-31";
+    datePicker.max = "2020-11-28";
+}
+
 function main(){
     let addPlantBtn = document.getElementById("addPlantBtn");
     addPlantBtn.addEventListener("click", addPlant);
+
+    setDateRestrictions();
 }
 
 main();
