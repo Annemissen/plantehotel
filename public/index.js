583cccb3f452f7e7e814bba51252c81843d3c420
GrothenKek <65117443+GrothenKek@users.noreply.github.com>
2020-11-12 09:19:04 +0100

    test af post

--- public/index.js
+++ public/index.js
@@ -11,23 +11,23 @@ const Address_label = document.getElementById("Address-label");
 
 
 const createCustomer = async () => {
-    let firstname = document.getElementById("name-input").value
-    let lastname = document.getElementById("lastname-input").value
-    let address = document.getElementById("address-input").value
-    let city = document.getElementById("city-input").value
-    let postalcode = document.getElementById("zip-code-input").value
-    let mobile = document.getElementById("number-input").value
-    let email = document.getElementById("email-input").value
-    //let plantcount = document.getElementById("numberOfPlants").value
+    let firstname = document.getElementById("name-input").value;
+    let lastname = document.getElementById("lastname-input").value;
+    let address = document.getElementById("address-input").value;
+    let city = document.getElementById("city-input").value;
+    let postalcode = document.getElementById("zip-code-input").value;
+    let mobile = document.getElementById("number-input").value;
+    let email = document.getElementById("email-input").value;
+    //let plantcount = document.getElementById("numberOfPlants").value;
 
-    let newcustomer = await fetch("api/customer",{
+    let newcustomer = await fetch("api/customer", {
         method: "POST",
-        headers: { "Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             firstname: firstname,
             lastname: lastname,
             address: address,
-            city : city,
+            city: city,
             postalcode: postalcode,
             mobile: mobile,
             email: email,
