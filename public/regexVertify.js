de0a9a7088d0739f5ee0c0f9a7143da860f30e53
Steffen Haunstrup <62333253+stef6593@users.noreply.github.com>
2020-11-19 09:27:50 +0100

    Opdate in regex

--- public/regexVertify.js
+++ public/regexVertify.js
@@ -1,65 +1,49 @@
 const elements = document.querySelectorAll('input,button');
 elements[12].onclick = () => {
-        //fornavn
-        if (!/^[a-zA-Zæøå]+$/.test(elements[0].value))
-            elements[0].style.backgroundColor = "red"
-        else if (/^[a-zA-Zæøå]+$/.test(elements[0].value))
-            elements[0].style.backgroundColor = "#EDDCD2"
+    //fornavn
+    if (!/^[a-zA-Zæøå]+$/.test(elements[0].value))
+        elements[0].style.backgroundColor = "red"
+    else if (/^[a-zA-Zæøå]+$/.test(elements[0].value))
+        elements[0].style.backgroundColor = "#EDDCD2"
 
-        //efternavn
-        if (!/^[a-zA-Zæøå]+$/.test(elements[1].value))
-            elements[1].style.backgroundColor = "red"
-        else if (/^[a-zA-Z]+$/.test(elements[1].value))
-            elements[1].style.backgroundColor = "#EDDCD2"
+    //efternavn
+    if (!/^[a-zA-Zæøå]+$/.test(elements[1].value))
+        elements[1].style.backgroundColor = "red"
+    else if (/^[a-zA-Z]+$/.test(elements[1].value))
+        elements[1].style.backgroundColor = "#EDDCD2"
 
-        //postnummer
-        if (!/^[0-9]{4}$/.test(elements[2].value))
-            elements[2].style.backgroundColor = "red"
-        else if (/^[0-9]{4}$/.test(elements[2].value))
-            elements[2].style.backgroundColor = "#EDDCD2"
+    //postnummer
+    if (!/^[0-9]{4}$/.test(elements[2].value))
+        elements[2].style.backgroundColor = "red"
+    else if (/^[0-9]{4}$/.test(elements[2].value))
+        elements[2].style.backgroundColor = "#EDDCD2"
 
-        //By
-        if (!/^[-\sa-zA-Zæøå]+$/.test(elements[3].value))
-            elements[3].style.backgroundColor = "red"
-        else if (/^[-\sa-zA-Zæøå]+$/.test(elements[3].value))
-            elements[3].style.backgroundColor = "#EDDCD2"
-            //vej og hus nr
+    //By
+    if (!/^[-\sa-zA-Zæøå]+$/.test(elements[3].value))
+        elements[3].style.backgroundColor = "red"
+    else if (/^[-\sa-zA-Zæøå]+$/.test(elements[3].value))
+        elements[3].style.backgroundColor = "#EDDCD2"
+        //vej og hus nr
 
-        if (!/^[-\sa-zA-Zæøå]+\s[0-9]$/.test(elements[4].value))
-            elements[4].style.backgroundColor = "red"
-        else if (/^[-\sa-zA-Zæøå]+\s[0-9]$/.test(elements[4].value))
-            elements[4].style.backgroundColor = "#EDDCD2"
-            //Mobil nummer (element 5)
-            /*
-            //uden mellemrum
-            /^[-\s0-9]{11}$/
-            */
+    if (!/^[-\sa-zA-Zæøå]+\s[0-9]$/.test(elements[4].value))
+        elements[4].style.backgroundColor = "red"
+    else if (/^[-\sa-zA-Zæøå]+\s[0-9]$/.test(elements[4].value))
+        elements[4].style.backgroundColor = "#EDDCD2"
+        //Mobil nummer (element 5)
+        /*
+        //uden mellemrum
+        /^[-\s0-9]{11}$/
+        */
 
-        //med mellemrum
-        if (!/^[0-9]{8}$/.test(elements[5].value))
-            elements[4].style.backgroundColor = "red"
-        else if (/^[0-9]{8}$/.test(elements[5].value))
-            elements[4].style.backgroundColor = "#EDDCD2"
+    //med mellemrum
+    if (!/^[0-9]{8}$/.test(elements[5].value))
+        elements[4].style.backgroundColor = "red"
+    else if (/^[0-9]{8}$/.test(elements[5].value))
+        elements[4].style.backgroundColor = "#EDDCD2"
 
-        //email
-        if (!/^[\w-æøå\.]+@([\w-æøå]+\.)+[\w-æøå]{2,4}$/.test(elements[6].value))
-            elements[6].style.backgroundColor = "red"
-        else if (/^[\w-æøå\.]+@([\w-æøå]+\.)+[\w-æøå]{2,4}$/.test(elements[6].value))
-            elements[6].style.backgroundColor = "#EDDCD2"
-    }
-    //plante navn?
-
-//Antal?
-
-//Eriks lektion 9.5 (skal slettes)
-/*
-const elements = document.querySelectorAll('input,button');
-elements[13].onclick = () => {
-    if (elements[0].value !== elements[1].value)
-        elements[3].innerHTML = 'Passwords er ikke ens!';
-    else if (/^.{8,}$/.test(elements[0].value))
-        elements[3].innerHTML = 'Password er ok!';
-    else
-        elements[3].innerHTML = 'Passwords er på mindre end 8 tegn!';
+    //email
+    if (!/^[\w-æøå\.]+@([\w-æøå]+\.)+[\w-æøå]{2,4}$/.test(elements[6].value))
+        elements[6].style.backgroundColor = "red"
+    else if (/^[\w-æøå\.]+@([\w-æøå]+\.)+[\w-æøå]{2,4}$/.test(elements[6].value))
+        elements[6].style.backgroundColor = "#EDDCD2"
 }
-*/
