0f8ccd4b26e48051365aebbd4f8dbc7a8ef75fff
mads110100100 <38919839+mads110100100@users.noreply.github.com>
2020-11-19 11:45:10 +0100

    forsøg på at få det til at virke med RegX

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,14 @@
+//const elements = document.querySelectorAll('input,button');
+// const firstnameInputField   = document.getElementById("name-input");
+// const lastanameInputField   = document.getElementById("lastname-input");
+// const zipInputField  = document.getElementById("zip-code-input");
+// const cityInputField  = document.getElementById("city-input");
+// const addressInputField   = document.getElementById("address-input");
+// const numberInputField   = document.getElementById("number-input");
+// const emailInputField  = document.getElementById("email-input");
+// const plantNameInputField  = document.getElementById("plantName");
+// const numberOfPlantsInputField  = document.getElementById("numberOfPlants");
+
 const mongoose = require('mongoose');
 const model = require('../models/models')
 const { firstname, lastname, address, postalcode, city, plantname, plantcount, pickup, mobile, email } = model;
@@ -5,7 +16,7 @@ const config = require('../config');
 const customers = require('../models/models.js')
 const fetch = require('node-fetch');
 const { databaseURI } = require('../config');
-
+const test = require('../public/regexVertify.js')
 
 
 
@@ -22,8 +33,11 @@ async function get(url) {
 }
 
 exports.createCustomer = async function (body) {
+   
+    await testRegX();
+    if(await testRegX()){
     const { firstname, lastname, address, postalcode, city, mobile, email } = body;
-
+    
     let res = await customers.information.create({
         firstname: firstname,
         lastname: lastname,
@@ -36,10 +50,83 @@ exports.createCustomer = async function (body) {
     });
     return res;
 }
+}
 
 exports.getCustomers = async function(){
     return await model.information.find();
 }
+
+
+// exports.testRegX  =async function (){
+//     //fornavn
+//     let bolian = true;
+
+//     if (!/^[a-zA-Zæøå]+$/.test(firstnameInputField.value)){
+//     firstnameInputField.style.backgroundColor = "red"
+//     bolian = false;
+//     }
+//     else if (/^[a-zA-Zæøå]+$/.test(firstnameInputField.value))
+//     firstnameInputField.style.backgroundColor = "#EDDCD2"
+
+//     //efternavn
+//     if (!/^[a-zA-Zæøå]+$/.test(lastanameInputField.value)){
+//     lastanameInputField.style.backgroundColor = "red"
+//     bolian = false;
+// }
+
+//     else if (/^[a-zA-Z]+$/.test(lastanameInputField.value))
+//     lastanameInputField.style.backgroundColor = "#EDDCD2"
+
+//     //postnummer
+//     if (!/^[0-9]{4}$/.test(zipInputField.value)){ 
+//     zipInputField.style.backgroundColor = "red"
+//     bolian = false;    
+//     }
+//     else if (/^[0-9]{4}$/.test(zipInputField.value))
+//     zipInputField.style.backgroundColor = "#EDDCD2"
+
+//     //By
+//     if (!/^[-\sa-zA-Zæøå]+$/.test(cityInputField.value)){
+//         cityInputField.style.backgroundColor = "red"
+//         bolian = false;
+//     }
+//     else if (/^[-\sa-zA-Zæøå]+$/.test(cityInputField.value))
+//         cityInputField.style.backgroundColor = "#EDDCD2"
+//         //vej og hus nr
+
+//     if (!/^[-\sa-zA-Zæøå]+\s[0-9] $/.test(addressInputField.value)){
+     
+//         addressInputField.style.backgroundColor = "red"
+//         bolian = false;
+//     }
+//     else if (/^[-\sa-zA-Zæøå]+\s[0-9]$/.test(addressInputField.value))
+//         addressInputField.style.backgroundColor = "#EDDCD2"
+//         //Mobil nummer (element 5)
+//         /*
+//         //uden mellemrum
+//         /^[-\s0-9]{11}$/
+//         */
+
+//     //med mellemrum
+//     if (!/^[0-9]{8}$/.test(numberInputField.value)){
+//         numberInputField.style.backgroundColor = "red"
+//         bolian = false;
+//     }
+//         else if (/^[0-9]{8}$/.test(numberInputField.value))
+//         numberInputField.style.backgroundColor = "#EDDCD2"
+
+//     //email
+//     if (!/^[\w-æøå\.]+@([\w-æøå]+\.)+[\w-æøå]{2,4}$/.test(emailInputField.value)){
+//         emailInputField.style.backgroundColor = "red"
+//         bolian = false;
+//     }
+//     else if (/^[\w-æøå\.]+@([\w-æøå]+\.)+[\w-æøå]{2,4}$/.test(emailInputField.value)){
+//         emailInputField.style.backgroundColor = "#EDDCD2";
+//     }
+//         return bolian;
+//     }
+
+
 
 //address: address,
 //postalcode: postalcode,
