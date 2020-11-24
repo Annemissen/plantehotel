297506d5d5bb852d9613ef0058f99c2f91cfcf98
Marcus Berg Andersen <MarcusBerg@live.dk>
2020-11-24 09:25:52 +0100

    reservations endpoint added to root

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const config = require('./config')
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use('/api/customers', require('./routes/planteHotel.js'));
+app.get('/reservations', function(req,res){
+    res.sendFile(__dirname+'/public/reservations.html')
+})
 
 const port = process.env.PORT || config.localPort; // Heroku
 app.listen(port);
